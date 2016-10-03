ccec2d9f14d46face39dba6cf258231e966ee283
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2016-10-03 15:47:16 +0200

    Make it a server

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!!!"))
+}
 
 func main() {
 	fmt.Println("hello world")
+	http.HandleFunc("/", HelloWorld)
+	http.ListenAndServe(":80", nil)
 }
