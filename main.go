a7f0f13379c9d79b2373b0307e1ae5cfc09fb974
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2016-10-10 16:31:52 +0200

    Connect to database

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"os"
 )
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!!!"))
+	w.Write([]byte("Hello World!!!\n"))
+
+	dburl := fmt.Sprintf("%s://%s:%s@%s:%s/%s", os.Getenv("DB_ENGINE"), os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	fmt.Fprintf(w, "Connect to %s\n", dburl)
+	db, err := sql.Open("postgres", dburl)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s\n", err)
+		return
+	}
+	fmt.Fprintf(w, "Connected.\n")
+	_ = db
 }
 
 func main() {
